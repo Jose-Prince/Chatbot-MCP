f121f853b59305428e87a0503ee46b85756bac42
Jose-Prince <joseprince148@gmail.com>
2025-08-26 21:08:19 -0600

    build: change directory distribution

--- frontend/main.lua
+++ frontend/main.lua
@@ -1,11 +1,14 @@
 local loveFrames = require("LoveFrames/loveframes")
+local width = love.graphics.getWidth()
+local height = love.graphics.getHeight()
 
 function love.load()
     local frame = loveFrames.Create("frame")
     frame:SetName("Example")
-    frame:SetSize(300, 200)
+    frame:SetSize(width, height)
+    frame:ShowCloseButton(false)
     frame:CenterWithinArea(0, 0, love.graphics.getWidth(), love.graphics.getHeight())
-    
+
     local button = loveFrames.Create("button", frame)
     button:SetText("Press me")
     button:SetPos(50, 50)
